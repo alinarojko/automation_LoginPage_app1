b1ffc76b26d746d1d1b36a10473921ff5fc22300
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-05-05 12:25:48 +0100

    Add option for multybrowser testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,27 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or edge)")
+
 
 @pytest.fixture()
-def driver():
-    print("Creating Chrome Driver")
-    my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} Driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser == "edge":
+        my_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    else:
+        raise TypeError(f"Expected 'chrome' or 'edge', but got {browser}")
     yield my_driver
     my_driver.quit()
-    print("Closing Chrome Driver")
-
+    print(f"Closing {browser} Driver")
