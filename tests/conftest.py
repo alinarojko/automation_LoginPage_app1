4d8d8416dd598c65b9e5e5cc7c2b28610a009c25
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-05-08 09:36:35 +0100

    Implicit and explicit waits , Exceptions and debuging

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.edge.service import Service as EdgeService
@@ -12,10 +13,11 @@ def pytest_addoption(parser):
         "--browser", action="store", default="chrome", help="browser to execute tests (chrome or edge)")
 
 
-@pytest.fixture(params=["chrome", "edge"])
+# @pytest.fixture(params=["chrome", "edge"])
+@pytest.fixture
 def driver(request):
-    # browser = request.config.getoption("--browser")
-    browser = request.param
+    browser = request.config.getoption("--browser")
+    # browser = request.param
     print(f"Creating {browser} Driver")
     if browser == "chrome":
         my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -23,6 +25,7 @@ def driver(request):
         my_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
     else:
         raise TypeError(f"Expected 'chrome' or 'edge', but got {browser}")
+    # my_driver.implicitly_wait(10)  # Implicit wait to let webdriver to wait for the element to appear on the page before failing the test
     yield my_driver
     my_driver.quit()
     print(f"Closing {browser} Driver")
